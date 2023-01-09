f4c2dd51c3022ced3bae1ab107653c10151f9c47
Fabio Oshiro <fabio.oshiro@gmail.com>
2023-01-09 18:58:34 -0300

    add scripts to help the build process

---
+++ build-cartesi-prod-mode.sh
@@ -0,0 +1,14 @@
+
+set -e
+cd ../
+export ROLLUPS_EXAMPLES=`pwd`
+cd -
+
+docker run \
+    -v `pwd`:/workdir \
+    -v $ROLLUPS_EXAMPLES:/rollups-examples \
+    -w /workdir \
+    --rm \
+    cartesi/toolchain:0.11.0 \
+    ./build-and-copy.sh
+
