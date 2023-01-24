dbb2c4130fb432901b2583013e7722f8d7a45162
Bruno Ochotorena <hackerhilt@gmail.com>
2023-01-24 19:08:14 -0300

    feat: add transition

--- app/src/components/NFTCard.vue
+++ app/src/components/NFTCard.vue
@@ -24,6 +24,6 @@ async function setPFP() {
 <template>
     <li class="rounded-md">
         <ProfileImage :image="image.src" />
-        <button class="cursor-pointer m-auto mt-3 text-white px-9 py-2.5 rounded-full font-semibold bg-pink-500 hover:bg-blue-500" @click="setPFP">Choose as PFP</button>
+        <button class="cursor-pointer m-auto mt-3 text-white px-9 py-2.5 rounded-full font-semibold bg-pink-500 hover:bg-blue-500 transition-colors duration-500" @click="setPFP">Choose as PFP</button>
     </li>
 </template>
